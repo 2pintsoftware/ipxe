3abba628cfc9770e15ac9f7bb2b9e8ce6e1946ba
Christian I. Nilsson <ChristianN@2PintSoftware.com>
2026-02-19 11:51:01 +0100

    [build] Update console config

--- src/config/2Pint/console.h
+++ src/config/2Pint/console.h
@@ -1,7 +1,20 @@
-#if defined ( PLATFORM_pcbios )
-#define	CONSOLE_SYSLOGS		/* Encrypted syslog console */
-#define	CONSOLE_FRAMEBUFFER	/* Graphical framebuffer console */
+#undef LOG_LEVEL
+#define LOG_LEVEL LOG_ALL
+
+#if defined ( PLATFORM_SYSLOG )
+/* Disable unsecured syslog messages */
+//#undef	CONSOLE_SYSLOG		/* Syslog console */
 #endif
-//#undef CONSOLE_SYSLOG ?
+
+#if ! defined ( PLATFORM_SYSLOGS )
+#define	CONSOLE_SYSLOGS		/* Encrypted syslog console */
+#endif
+
+/* when FB mode we don't want DEBUG output anymore, before that DEBUG is shown as well */
+#if defined ( CONSOLE_FRAMEBUFFER )
+#undef CONSOLE_FRAMEBUFFER
+#endif
+#define CONSOLE_FRAMEBUFFER ( CONSOLE_USAGE_ALL & ~CONSOLE_USAGE_LOG & ~CONSOLE_USAGE_DEBUG ) /* Graphical framebuffer console */
+
 #undef	KEYBOARD_MAP
 #define KEYBOARD_MAP dynamic
